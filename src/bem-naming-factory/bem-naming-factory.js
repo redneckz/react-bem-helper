72ce38bf745da0eca461b2fece0a75b3751266a8
Alexandr Shushunov <ashushunov@mail.ru>
2017-04-26 20:19:46 +0300

    Function which as created by createModifiersMapper handles incorrect inputs
    Move createModifiersMapper to a separate file.
    Add tests.

--- src/bem-naming-factory/bem-naming-factory.js
+++ src/bem-naming-factory/bem-naming-factory.js
@@ -1,13 +1,6 @@
-import isString from 'lodash/isString';
-import isPlainObject from 'lodash/isPlainObject';
-import isArray from 'lodash/isArray';
-import mapKeys from 'lodash/mapKeys';
-import compact from 'lodash/compact';
-import kebabCase from 'lodash/kebabCase';
 import {Config} from '../config';
 import {assertNamePart} from '../bem-naming-validators';
-
-const {ELEMENT_SEPARATOR, MODIFIER_SEPARATOR} = Config;
+import {createModifiersMapper} from './modifiers-mapper-creator';
 
 export function createBlockNameFactory(block) {
     assertNamePart(block);
@@ -17,20 +10,5 @@ export function createBlockNameFactory(block) {
 export function createElementNameFactory(block, element) {
     assertNamePart(block);
     assertNamePart(element);
-    return createModifiersMapper(`${block}${ELEMENT_SEPARATOR}${element}`);
-}
-
-function createModifiersMapper(name) {
-    return function mapModifier(modifier) {
-        if (modifier && isString(modifier)) {
-            const adjustedModifier = kebabCase(modifier);
-            assertNamePart(adjustedModifier);
-            return `${name}${MODIFIER_SEPARATOR}${adjustedModifier}`;
-        } else if (isPlainObject(modifier)) {
-            return mapKeys(modifier, (value, key) => mapModifier(key));
-        } else if (isArray(modifier)) {
-            return compact(modifier).map(mapModifier);
-        }
-        return name;
-    };
+    return createModifiersMapper(`${block}${Config.ELEMENT_SEPARATOR}${element}`);
 }
