810674fe7179821f6cb25b9f41c31114648af7cc
Alexander Alexandrov <aalexandrovv@gmail.com>
2018-02-17 14:17:17 +0300

    Fixed warning in block integrational test

--- src/block/block.integrational.spec.jsx
+++ src/block/block.integrational.spec.jsx
@@ -3,6 +3,7 @@ import Enzyme from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import {block} from './block';
 import {element, transparent} from '../element';
+import {div} from '../tag';
 import {Config} from '../config';
 
 const {ELEMENT_SEPARATOR, MODIFIER_SEPARATOR} = Config;
@@ -10,14 +11,9 @@ const {ELEMENT_SEPARATOR, MODIFIER_SEPARATOR} = Config;
 const {mount} = Enzyme;
 Enzyme.configure({adapter: new EnzymeAdapter()});
 
-
 describe('BEM block decorator', () => {
-    let Foo;
-    beforeEach(() => {
-        Foo = props => <div {...props} />;
-    });
-
     it('should provide BEM element decorator bound to block', () => {
+        const Foo = () => null;
         const WrappedFoo = block('foo')(Foo);
         expect(Foo.element).toBeInstanceOf(Function);
         expect(WrappedFoo.element).toBeInstanceOf(Function);
@@ -25,6 +21,7 @@ describe('BEM block decorator', () => {
 
     describe('composed with BEM element', () => {
         it('should provide block name, block modifiers and block styles by means of context', () => {
+            const Foo = div();
             const WrappedFoo = block('foo', ({quux}) => ({quux}))(Foo);
             const Bar = props => <div {...props} />;
             const WrappedBar = element('bar', transparent())(Bar);
